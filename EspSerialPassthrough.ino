c15a65c39da8696cfe8f69dcc20e194df25d5713
Bruno Figueiredo <bruno@hunterboards.com>
2022-06-30 13:14:48 +0100

    Fix/Remove Comments and Add new ports

--- EspSerialPassthrough.ino
+++ EspSerialPassthrough.ino
@@ -1,36 +1,3 @@
-﻿/*
-   EspSerialPassthrough v1.0
-   ESP8266 / ESP-01 Serial Passthrough for flashing using Arduino SAMD MCU
-
-   This Arduino sketch allows you to send AT commands, watch bootloader messages, or flash an ESP-01/ESP8266 through a SAMD21/SAMD51 or similar MCU. 
-   The MCU should be operating at 3.3V, if not, voltage level shifters must be used on the connections to ESP.
-   The program might also work on other ESPs, but have only been tested with ESP8266 in the form of a ESP-01 board.
-   
-   Developed and tested with a Arduino MKR board, SAMD21-based.
-   The program might work on MCUs without native USB, but no guarantees! :-)
-
-   Sending AT commands has been tested using the Arduino serial monitor, Visual Micro serial monitor and Putty in serial mode.
-   The same goes for watching bootloader output.
-   Firmware upgrade/flashing has been done with the official ESP8266 Flash Download Tool from https://www.espressif.com/en/support/download/other-tools, 
-   with firmware downloaded from https://www.espressif.com/en/support/download/at
-
-  Copyright 2021 Kåre Smith (Kaare Smith)
-
-  Licensed under the Apache License, Version 2.0 (the "License");
-  you may not use this file except in compliance with the License.
-  You may obtain a copy of the License at
-
-    http://www.apache.org/licenses/LICENSE-2.0
-
-  Unless required by applicable law or agreed to in writing, software
-  distributed under the License is distributed on an "AS IS" BASIS,
-  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-  See the License for the specific language governing permissions and
-  limitations under the License.
-
-*/
-
-// Arduino SAMD MCUs have native USB, but others might have native USB as well. If so: define HAVE_NATIVEUSB manually
 #ifdef SERIAL_PORT_USBVIRTUAL
 #define HAVE_NATIVEUSB
 #define SERIAL_WRITE_BUFFER_SIZE 16384
@@ -90,9 +57,9 @@ uint32_t espBaud = passthroughMode == AtCommandsMode ? AT_MODE_BAUD : passthroug
 #define esp8266 Serial1
 
 // The pin used on the Arduino board. Important: must be 3.3V GPIO pins, or use voltage level shifter
-#define CH_PD_PIN 8  // Arduino pin connected to CH_PD (sometimes marked EN) pin on ESP8266
-#define GPIO0_PIN 9  // Arduino pin connected to GPIO0 pin on ESP8266
-#define RESET_PIN 10 // Arduino pin connected to RESET pin on ESP8266
+#define CH_PD_PIN 27  // Arduino pin connected to CH_PD (sometimes marked EN) pin on ESP8266
+#define GPIO0_PIN 4  // Arduino pin connected to GPIO0 pin on ESP8266
+#define RESET_PIN 7 // Arduino pin connected to RESET pin on ESP8266
 // Arduino GND is connected to ESP GND
 // Arduino VCC (3.3V) is connected to ESP8266 3V3
 // Arduino TX is connected to ESP8266 RX
